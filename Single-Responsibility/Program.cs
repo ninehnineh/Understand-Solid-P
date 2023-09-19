6bfccbea889bcc0ff7262339279dfe5f0b35d432
ninehnineh <103179810+ninehnineh@users.noreply.github.com>
2023-09-20 01:04:23 +0700

    Add project files.

---
+++ Single-Responsibility/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Single_Responsibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var cart = new ShoppingCartServices();
+
+            //AddProductToCart(cart);
+
+            //PrintCart(cart);
+
+            //SaveToFile(cart);
+        }
+
+        private static void SaveToFile(ShoppingCartServices cart)
+        {
+            Console.WriteLine();
+            Console.WriteLine("====== Save To File ======");
+            Console.WriteLine("Writing to file...");
+            cart.SaveToFile("cart.json");
+        }
+
+        private static void PrintCart(ShoppingCartServices cart)
+        {
+            Console.WriteLine();
+            Console.WriteLine("====== Print cart ======");
+            cart.Print(); 
+        }
+
+        private static void AddProductToCart(ShoppingCartServices cart)
+        {
+            Console.WriteLine("====== Add product to cart ======");
+            cart.Add(new ShoppingCartModel()
+            {
+                Price = 1200,
+                ProductId = 1,
+                Quantity = 1,
+                Title = "iPhone 14"
+            });
+            cart.Add(new ShoppingCartModel()
+            {
+                Price = 1400,
+                ProductId = 1,
+                Quantity = 2,
+                Title = "iPhone 15"
+            });
+            Console.WriteLine("Add Success...");
+
+        }
+    }
+}
---
+++ Single-Responsibility/ShoppingCartServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility
+{
+    internal class ShoppingCartServices
+    {
+        private List<ShoppingCartModel> items = new List<ShoppingCartModel>();
+
+        public IEnumerable<ShoppingCartModel> Items { get { return items; } }
+
+        public void Add(ShoppingCartModel item) => items.Add(item);
+
+        public void Remove(ShoppingCartModel item) => items.Remove(item);
+
+        public double GetTotal() => items.Sum(i => i.Quantity * i.Price);
+
+        public void Clear() => items.Clear();
+
+        public void Print()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.Title} - {item.Price:C} x {item.Quantity} = {item.Quantity * item.Price:C}");
+            }
+        }
+
+        public void SaveToFile(string fileName) => Console.WriteLine("Write file success...");
+
+        public void LoadFromFile(string fileName) => Console.WriteLine("Load file success...");
+    }
+}
